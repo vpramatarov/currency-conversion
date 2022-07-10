4831898d506e1287f17fbf850bcc538986d07c4a
vpramatarov <velizarpamatarov@gmail.com>
2022-07-10 19:23:39 +0300

    install API Platform & Doctrine

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
